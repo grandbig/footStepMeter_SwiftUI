0d7e891cd796003e227db0ef12f6b2034b7404dd
grandbig <takahiro.kato15@gmail.com>
2024-05-18 21:20:36 +0900

    UIToolbarの有効/無効状態による背景色の切り替えおよびタップ時の処理対応を追加しました。

--- MapSample/ContentView.swift
+++ MapSample/ContentView.swift
@@ -15,32 +15,36 @@ struct ContentView: View {
         NavigationStack {
             MapView()
             .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(Color("main"), for: .navigationBar, .bottomBar)
-            .toolbar(.visible, for: .navigationBar, .bottomBar)
+            .toolbarBackground(Color("main"), for: .navigationBar)
+            .toolbar(.visible, for: .navigationBar)
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
                     VStack {
                         Image("play")
+                            .setUpToolbarImageStyle(isEnabled: true, onTapGesture: {})
                         Text("START")
-                            .font(.footnote)
+                            .setUpToolbarTextStyle(isEnabled: true, onTapGesture: {})
                     }
                     Spacer()
                     VStack {
                         Image("stop")
+                            .setUpToolbarImageStyle(isEnabled: false, onTapGesture: {})
                         Text("STOP")
-                            .font(.footnote)
+                            .setUpToolbarTextStyle(isEnabled: false, onTapGesture: {})
                     }
                     Spacer()
                     VStack {
                         Image("view")
+                            .setUpToolbarImageStyle(isEnabled: true, onTapGesture: {})
                         Text("FOOT VIEW")
-                            .font(.footnote)
+                            .setUpToolbarTextStyle(isEnabled: true, onTapGesture: {})
                     }
                     Spacer()
                     VStack {
                         Image("settings")
+                            .setUpToolbarImageStyle(isEnabled: true, onTapGesture: {})
                         Text("SETTINGS")
-                            .font(.footnote)
+                            .setUpToolbarTextStyle(isEnabled: true, onTapGesture: {})
                     }
                 }
             }
@@ -51,12 +55,36 @@ struct ContentView: View {
     init() {
         setUpToolbarBackgroundColor()
     }
-
+    
+    /// UIToolbarの背景色を設定する。
+    /// - Note: toolbarBackground では「.bottomBar」を指定しても背景色を変更できないため、この処理を実行する
     private func setUpToolbarBackgroundColor() {
         let appearance = UIToolbarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = UIColor(Color("main"))
         UIToolbar.appearance().scrollEdgeAppearance = appearance
+    }
+}
+
+extension Image {
+
+    func setUpToolbarImageStyle(isEnabled: Bool, onTapGesture: @escaping () -> Void) -> some View {
+        self.renderingMode(.template)
+            .foregroundStyle(isEnabled ? .white : .gray)
+            .onTapGesture {
+                onTapGesture()
+            }
+    }
+}
+
+extension Text {
+
+    func setUpToolbarTextStyle(isEnabled: Bool,onTapGesture: @escaping () -> Void) -> some View {
+        self.font(.footnote)
+            .foregroundStyle(isEnabled ? .white : .gray)
+            .onTapGesture {
+                onTapGesture()
+            }
     }
 }
 
